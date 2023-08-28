cdd1cdc04ff54c6dc6a5051324434c416e3628bc
phonosync <24829638+phonosync@users.noreply.github.com>
2023-08-28 10:42:44 +0200

    distance matrix code goes here

--- app.py
+++ app.py
@@ -1,117 +1,69 @@
 import streamlit as st
-from pathlib import Path
 import io
-import numpy as np
 import pandas as pd
-from PIL import Image
-import openpyxl
+from sklearn.metrics import pairwise_distances
 
-output_fn = None
+st.title("Pairwise distance matrix")
 
-st.title("Convert images to xlsx")
+model = None
 
-st.write('''The pixel intensities from the image file(s) are extracted into an xlsx-file. If a single image is provided, a separate 
-         worksheet is created for each channel with the corresponding image height x width pixel intensities in a matrix. Greyscale
-         images result into one worksheet only. \\
-         If multiple image files are uploaded, only one sheet is created with all the pixel intensities for the images 
-         (and their color channels) reshaped into individual rows indexed by the original filenames. Attention: Unequal pixel
-         dimensions of the images leads to inconsistent number of columns in the excel sheet.
-         ''')
-
-to_greyscale = st.checkbox('Convert to greyscale')
-
-# sheet_per_channel = st.checkbox('arrays of multi-channel input images will be split into separate worksheets of the resulting Excel (one worksheet per channel)')
-
-# reshape_to_row = st.checkbox('Reshape the image array to one row in resulting xlsx-Worksheet. Automatic behaviour for multi-images upload')
-
-scale_to_width = st.number_input('Scale to width', value=0)
-scale_to_height = st.number_input('Scale to height', value=0)
-
-
-uploaded_files = st.file_uploader("Choose one or more image files", accept_multiple_files=True)
-if len(uploaded_files) > 1:
-    reshape_to_row = True
-
-
-# create xlsx
-
-#if single image: one sheet per channel, 2d-matrix height x width pixels
-#else: one sheet. one row per file, channels appended
-
-if len(uploaded_files) == 1:
-    img = Image.open(uploaded_files[0])
-    if to_greyscale:
-        img = img.convert('L')
+if not model:
+    # select metric
+    metric = st.selectbox('Select distance metric to calculate',
+                                ['cityblock', 'cosine', 'euclidean', 'l1', 'l2', 'manhattan']
+                                )
     
-    if scale_to_width > 0 and scale_to_height > 0:
-        img = img.resize((scale_to_width, scale_to_height),               # Tuple representing size
-                        resample=None,      # Optional resampling filter
-                        box=None,           # Optional bounding box to resize
-                        reducing_gap=None   # Optional optimization
-                        )
+    st.write("Upload a csv file with the data. One sample per row. 1st row is assumed to hold variable names.")
+    df_sample_inp = pd.DataFrame({'Variable 1': [0.0, 4.1, 2.3], 'Variable 2': [0, 1, 1], 'Variable 3': [5, 3, 9]})
+    st.markdown(df_sample_inp.style.hide(axis="index").to_html(), unsafe_allow_html=True)
+
+
+    st.text("")
+    st.text("")
+    st.write("It is possible to provide sample indeces in the first column of the uploaded file. They will be used to label columns and rows in the resulting distance matrix. Indicate in the following checkbox:")
     
-    bands = img.getbands() # ('R', 'G', 'B') and for a typical gray-scale image would be ('L',).
+    df_sample_inp = pd.DataFrame({'Index': ['Sample 1', 'Sample 2', 'Sample 3'], 'Variable 1': [0.0, 4.1, 2.3], 'Variable 2': [0, 1, 1], 'Variable 3': [5, 3, 9]})
+    st.markdown(df_sample_inp.style.hide(axis="index").to_html(), unsafe_allow_html=True)
+    
+    index_column = st.checkbox('1st column in uploaded file contains sample indeces')
+    header_row = st.checkbox('1st row contains column labels') 
 
-    arr = np.asarray(img) # shape: (height, width, n_channels)
-
-    st.write(bands)
-    st.write(arr.shape)
-
-    buffer = io.BytesIO()
-    with pd.ExcelWriter(buffer) as writer:
-        if len(bands) == 1:
-            df_tmp = pd.DataFrame(data=arr[:,:]) # ,band_idx
-            df_tmp.to_excel(writer, sheet_name=bands[0], index=False, header=False)
+    inp_file = st.file_uploader("Choose a file")
+    if inp_file is not None:
+        
+        if index_column:
+            index_col = 0
         else:
-            for band_idx, band_label in enumerate(bands):
-                df_tmp = pd.DataFrame(data=arr[:,:,band_idx])
-                df_tmp.to_excel(writer, sheet_name=band_label, index=False, header=False)
+            index_col = None
+        if header_row:
+            header = 0
+        else:
+            header = None
+        df = pd.read_csv(inp_file, delimiter=';', index_col=index_col, header=header)
 
-    output_fn = Path(uploaded_files[0].name).stem + '.xlsx'
+        # if index_column:
+        #     df.set_index(df.columns[0],inplace=True)
 
-if len(uploaded_files) > 1:
+        X = df.to_numpy(copy=True)
 
-    fns = []
-    pixel_arrays = []
-    
-    for uploaded_file in uploaded_files:
-        img = Image.open(uploaded_file)
-        if to_greyscale:
-            img = img.convert('L')
-        
-        if scale_to_width > 0 and scale_to_height > 0:
-            img = img.resize((scale_to_width, scale_to_height),               # Tuple representing size
-                            resample=None,      # Optional resampling filter
-                            box=None,           # Optional bounding box to resize
-                            reducing_gap=None   # Optional optimization
-                            )
-        
-        # bands = img.getbands() # ('R', 'G', 'B') and for a typical gray-scale image would be ('L',).
+        y_hat = pairwise_distances(X, metric=metric)
 
-        arr = np.asarray(img) # shape: (height, width, n_channels)
-        st.write(arr.shape)
-        n_pixels = 1
-        for val in arr.shape:
-            n_pixels *= val
-        # st.write(arr.reshape(n_pixels).shape)
-        
-        fns.append(uploaded_file.name)
-        pixel_arrays.append(list(arr.reshape(n_pixels)))
+        df_dists = pd.DataFrame(data=y_hat,
+                                index=df.index,
+                                columns=df.index
+                                )
 
-    buffer = io.BytesIO()
-    wb = openpyxl.Workbook()
-    ws = wb.worksheets[0]
+        df_pars = pd.DataFrame([['metric', 'date'],[metric, 'today']])
 
-    for fn, p_array in zip(fns, pixel_arrays):
-        ws.append([fn] + p_array)
+        buffer = io.BytesIO()
+        with pd.ExcelWriter(buffer) as writer:  
+            df_dists.to_excel(writer, sheet_name='Distance Matrix', index=True)
+            df_pars.to_excel(writer, sheet_name='Calculation details', index=False)
 
-    wb.save(buffer)
-    output_fn = 'pixel_intensities.xlsx'
 
-if output_fn:
-    st.download_button(
-            label="Download pixel intensities as xlsx",
+        st.download_button(
+            label="Download pairwise distance matrix as xlsx",
             data=buffer,
-            file_name=output_fn,
+            file_name='distance_matrix.xlsx',
             mime='application/vnd.ms-excel',
         )
