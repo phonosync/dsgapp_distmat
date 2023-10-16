8cb46ae095216624243ba0acd844fb5aa457d4a5
phonosync <24829638+phonosync@users.noreply.github.com>
2023-10-16 17:42:29 +0200

    Changes to be committed: modified:   app.py

--- app.py
+++ app.py
@@ -3,32 +3,33 @@ import io
 import pandas as pd
 from sklearn.metrics import pairwise_distances
 
-st.title("Pairwise distance matrix")
+st.title("Erstellung der paarweisen Distanzmatrix")
 
 model = None
 
 if not model:
     # select metric
-    metric = st.selectbox('Select distance metric to calculate',
+    metric = st.selectbox('Wähle Distanz-Metrik',
                                 ['cityblock', 'cosine', 'euclidean', 'l1', 'l2', 'manhattan']
                                 )
     
-    st.write("Upload a csv file with the data. One sample per row. 1st row is assumed to hold variable names.")
+    st.write("Laden Sie ein csv-File mit den Daten hoch. Ein Sample pro Zeile. Die erste Reihe wird als Kopfzeile Variablen-Namen interpretier.")
     df_sample_inp = pd.DataFrame({'Variable 1': [0.0, 4.1, 2.3], 'Variable 2': [0, 1, 1], 'Variable 3': [5, 3, 9]})
     st.markdown(df_sample_inp.style.hide(axis="index").to_html(), unsafe_allow_html=True)
 
 
     st.text("")
     st.text("")
-    st.write("It is possible to provide sample indeces in the first column of the uploaded file. They will be used to label columns and rows in the resulting distance matrix. Indicate in the following checkbox:")
+    st.write("""Sample-Indices können in der ersten Spalte des hochgeladenen Datei angegeben werden. 
+             Sie werden als Spalten- und Zeilen-Labels im Output-File verwendet. Wählen Sie in der folgenden Checkbox:""")
     
     df_sample_inp = pd.DataFrame({'Index': ['Sample 1', 'Sample 2', 'Sample 3'], 'Variable 1': [0.0, 4.1, 2.3], 'Variable 2': [0, 1, 1], 'Variable 3': [5, 3, 9]})
     st.markdown(df_sample_inp.style.hide(axis="index").to_html(), unsafe_allow_html=True)
     
-    index_column = st.checkbox('1st column in uploaded file contains sample indeces')
-    header_row = st.checkbox('1st row contains column labels') 
+    index_column = st.checkbox('Erste Spalte enthält Sample-Indices')
+    header_row = st.checkbox('Erste Zeile enthält Spalten-Labels') 
 
-    inp_file = st.file_uploader("Choose a file")
+    inp_file = st.file_uploader("Wählen Sie ein File")
     if inp_file is not None:
         
         if index_column:
@@ -62,7 +63,7 @@ if not model:
 
 
         st.download_button(
-            label="Download pairwise distance matrix as xlsx",
+            label="Distanzmatrix als xlsx-File herunterladen",
             data=buffer,
             file_name='distance_matrix.xlsx',
             mime='application/vnd.ms-excel',
