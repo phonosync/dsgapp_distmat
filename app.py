5c4797dba9d859c939845754d22f7e798d3a1e3d
phonosync <24829638+phonosync@users.noreply.github.com>
2023-10-17 02:45:38 +0200

    sniff delimiter

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import io
+import csv
 import pandas as pd
 from sklearn.metrics import pairwise_distances
 
@@ -14,17 +15,22 @@ if not model:
                                  'l2', 'manhattan']
                          )
     
-    st.write("Laden Sie ein csv-File mit den Daten hoch. Ein Sample pro Zeile. Die erste Reihe wird als Kopfzeile Variablen-Namen interpretier.")
-    df_sample_inp = pd.DataFrame({'Variable 1': [0.0, 4.1, 2.3], 'Variable 2': [0, 1, 1], 'Variable 3': [5, 3, 9]})
+    st.write('''Laden Sie eine csv-Datei mit den Daten hoch. Ein Sample pro 
+             Zeile. Die erste Reihe wird als Kopfzeile Variablen-Namen interpretiert.''')
+    df_sample_inp = pd.DataFrame({'Variable 1': [0.0, 4.1, 2.3], 'Variable 2': [0, 1, 1], 
+                                  'Variable 3': [5, 3, 9]})
     st.markdown(df_sample_inp.style.hide(axis="index").to_html(), unsafe_allow_html=True)
 
 
     st.text("")
     st.text("")
-    st.write("""Sample-Indices können in der ersten Spalte des hochgeladenen Datei angegeben werden. 
-             Sie werden als Spalten- und Zeilen-Labels im Output-File verwendet. Wählen Sie in der folgenden Checkbox:""")
+    st.write("""Sample-Indices können in der ersten Spalte des hochgeladenen Datei 
+             angegeben werden. Sie werden als Spalten- und Zeilen-Labels im Output-File 
+             verwendet. Wählen Sie in der folgenden Checkbox:""")
     
-    df_sample_inp = pd.DataFrame({'Index': ['Sample 1', 'Sample 2', 'Sample 3'], 'Variable 1': [0.0, 4.1, 2.3], 'Variable 2': [0, 1, 1], 'Variable 3': [5, 3, 9]})
+    df_sample_inp = pd.DataFrame({'Index': ['Sample 1', 'Sample 2', 'Sample 3'], 
+                                  'Variable 1': [0.0, 4.1, 2.3], 'Variable 2': [0, 1, 1], 
+                                  'Variable 3': [5, 3, 9]})
     st.markdown(df_sample_inp.style.hide(axis="index").to_html(), unsafe_allow_html=True)
     
     index_column = st.checkbox('Erste Spalte enthält Sample-Indices')
@@ -41,7 +47,12 @@ if not model:
             header = 0
         else:
             header = None
-        df = pd.read_csv(inp_file, delimiter=';', index_col=index_col, header=header)
+
+        with open(inp_file, newline='') as csvfile:
+            dialect = csv.Sniffer().sniff(csvfile.read())
+        
+        df = pd.read_csv(inp_file, dialect.delimiter, index_col=index_col, header=header)
+        
 
         # if index_column:
         #     df.set_index(df.columns[0],inplace=True)
